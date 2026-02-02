b1fabbe8245894890ddef85d2509f82f0941328b
mizanur2734 <mizanurbb2791@gmail.com>
2026-02-02 21:41:28 +0600

    more-arrow.js

---
+++ more-arrow.js
@@ -0,0 +1,11 @@
+// single parameter
+const getSquare = x => x * x;
+const getHalf = x => x / 2
+const firstElement = nums => nums[0];
+
+// console.log(getSquare(5));
+// console.log(getHalf(10));
+// console.log(firstElement([22, 33, 44])) 
+
+// anonimous function
+
