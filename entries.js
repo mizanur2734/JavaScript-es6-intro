ae9c3a5dd700f2c20f120062824465c1270a8b94
mizanur2734 <mizanurbb2791@gmail.com>
2026-02-04 21:22:49 +0600

    added the entries

---
+++ entries.js
@@ -0,0 +1,13 @@
+const emplayee = {
+    name: "John Doe",
+    position: "Software Engineer",
+    department: "Development",
+    startDate: "2022-01-15"
+}
+
+const keys = Object.keys(emplayee);
+const valus = Object.values(emplayee);
+const entries = Object.entries(emplayee);
+console.log(keys)
+console.log(valus)
+console.log(entries)
