ecea4060025c73b72b6f5dd9c0e0c95472bbcf0a
mizanur2734 <mizanurbb2791@gmail.com>
2026-02-06 11:54:32 +0600

    added the dot-bracket.js

---
+++ es6-intro/dot-bracket.js
@@ -0,0 +1,15 @@
+const student = {
+    first: 'John',
+    last: 'Doe',
+    age: 30,
+    1: 50,
+    'home address': '123 Main St'
+};
+
+// dotenoation
+const studentName1 = student.first
+
+// bracket notation
+const studentName2 = student['home address']
+console.log(studentName1)
+console.log(studentName2)
