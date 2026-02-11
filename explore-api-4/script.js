e1d05d4258b23cfab31f54b6fc1b366e76b31c71
mizanur2734 <mizanurbb2791@gmail.com>
2026-02-11 23:22:16 +0600

    added the load Post

--- explore-api-4/script.js
+++ explore-api-4/script.js
@@ -1,21 +1,41 @@
 const loadData = () => {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
-}
-
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+};
+//-----------------------------------------------------
 const loadPost = () => {
-    const url = "https://jsonplaceholder.typicode.com/posts";
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        displaypost(data)
-    })
-}
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data)
+      displaypost(data);
+    });
+};
 
-const displaypost = (posts) =>{
-    posts.forEach(post => {
-        console.log(post)
-    })
-}
+const displaypost = (posts) => {
+  // get the container
+  const postContainer = document.getElementById("post-container");
+  // console.log(postContainer)
+  postContainer.innerHTML = "";
+
+  posts.forEach((post) => {
+    // console.log(post.title)
+
+    // create a html element
+    const postCard = document.createElement("div");
+    postCard.innerHTML = `
+            <div class="post-card">
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+        </div>
+        `;
+    // console.log(postCard)
+
+    // append the element to the container
+    postContainer.appendChild(postCard);
+  });
+};
+
+loadPost();
